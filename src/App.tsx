59862e4834bf00e081f20baa8cb2cd0d16046e64
Jakub Dyoniziak <jakubdyoniziak@gmail.com>
2026-06-17 14:29:11 +0200

    Tworzenie projektu

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Generator wycen</h1>
+
+      <input
+        placeholder="Numer wyceny"
+      />
+
+      <br />
+      <br />
+
+      <input
+        placeholder="Tytuł wyceny"
+      />
+    </div>
+  );
+}
